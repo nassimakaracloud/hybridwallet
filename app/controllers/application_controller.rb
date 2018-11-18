c889ad33596f91151aa6ab5aabcfa67963a17178
Nassima Kara <nassimakara@icloud.com>
2018-11-18 01:59:50 +0000

    Update homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    
+    def after_sign_in_path_for(resource)  
+     home_home_path #edited
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,9 @@ class HomeController < ApplicationController
     api_client = BitcoinAverage::HTTP.new
     @bitcoin = JSON.parse api_client.ticker_data('global', 'BTCUSD').body
     @ethereum = JSON.parse api_client.ticker_data('global', 'ETHUSD').body
+    @litecoin = JSON.parse api_client.ticker_data('global', 'LTCUSD').body
+    @ZCash = JSON.parse api_client.ticker_data('global', 'ZECUSD').body
+   
   end
 
 end
