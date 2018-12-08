176cb09477e8f6da3f98d966753c215bb2e22a18
Nassima Kara <nassimakara@icloud.com>
2018-12-08 21:23:36 +0000

    Updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,5 +61,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
-  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'https://hybridwallet-nassimakara100.c9users.io/users/sign_in' }
 end
