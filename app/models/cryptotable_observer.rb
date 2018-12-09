bc29a2f9f6990c095fbcfebd7f7f70789cc3444d
Nassima Kara <nassimakara@icloud.com>
2018-12-09 04:17:49 +0000

    Updates

--- app/models/cryptotable_observer.rb
+++ app/models/cryptotable_observer.rb
@@ -2,7 +2,7 @@ class CryptotableObserver < ActiveRecord::Observer
 
   def after_save(record)
     my_logger = MyLogger.instance
-    my_logger.logInformation "Crytable has changed: #{record.symbol}"
+    my_logger.logInformation "Cryptotable has changed: #{record.symbol}"
   end
               
 end
